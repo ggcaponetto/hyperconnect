b5e47d45d85939e6fc533645a063ba7ff54163ea
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2019-03-14 00:07:05 +0100

    fix gitignore

---
+++ Assets/chat-example/src/hyperconnect-async.js
@@ -0,0 +1,5 @@
+console.log("hypeconnect async - start.");
+
+setInterval(function () {
+    console.log("hypeconnect async: it's " + new Date());
+}, 1000);
---
+++ Assets/chat-example/src/hyperconnect-sync.js
@@ -0,0 +1,1 @@
+console.log("hypeconnect sync - done.");
